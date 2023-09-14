5ccd650c6a6250a2c434aa1e27bb8e89651ff581
johnnyboiii3020 <janugadearchit.shivratna@gmail.com>
2023-09-14 21:27:43 +0530

    Reverse Linked List(Recursive)

---
+++ DS Algo 450 Quest/LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+using namespace std;
+
+//LinkedList Node
+struct Node
+{
+    struct Node* next;
+    int value;
+    Node(int _val)
+    {
+        this->value = _val;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+    Node* head;
+    
+    LinkedList() {head = NULL;}
+
+    //Iterative Approach
+    void ReverseIter()
+    {
+        Node* next = NULL;
+        Node* prev = NULL;
+        Node* current;
+        current = head;
+        while(current != NULL)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+
+
+        }
+        head = prev;
+
+
+    }
+    // 1->2->3->4->5
+    Node* reverseRecursive(Node* head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+ 
+        /* reverse the rest list and put
+          the first element at the end */
+        Node* rest = reverseRecursive(head->next);
+        head->next->next = head;
+ 
+        /* tricky step -- see the diagram */
+        head->next = NULL;
+ 
+        /* fix the head pointer */
+        return rest;
+    }
+    void print()
+    {
+        Node* printVal = head;
+        while(printVal != NULL)
+        {
+            cout<<printVal->value;
+            printVal = printVal->next;
+
+        }
+
+    }
+
+    void push(int val)
+    {
+        Node* temp = new Node(val);
+        temp->next = head;
+        head = temp;
+    }
+
+    int pull()
+    {
+        if(head != NULL){
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }       
+    }
+
+};
+
+
+
+
+
+int main()
+{
+    
+
+
+}
+
+
