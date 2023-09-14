1b440ea27a4bc745e9ecdfedac51a812cba8fac9
johnnyboiii3020 <janugadearchit.shivratna@gmail.com>
2023-09-14 09:23:59 +0530

    Spirally Traversed Matrix

--- DS Algo 450 Quest/SpirallyTraverseMatrix/SpirallyTraverseMatrix.cpp
+++ DS Algo 450 Quest/SpirallyTraverseMatrix/SpirallyTraverseMatrix.cpp
@@ -1,41 +1,86 @@
 #include<iostream>
 #include <vector>
 #include<algorithm>
+
 using namespace std;
 vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
 {
-    int currentTrav = 0;
-    int totalTrav = r*c;
-    int firsti = c-1;
-    int lastj = r-1;
-    int i =0;
-    int j = 0;
-    vector<int> ans;
-    while(currentTrav < totalTrav)
-    {
-        //currentTrav++;
-        while(j < lastj)
-        {
-            ans.push_back(matrix[i][j]);
-            j++;
-        }
-        lastj = j;
-        while(i < firsti)
-        {
-            ans.push_back(matrix[i][j]);
-            i++;
-        }
+    vector<int> res;
+    int jCounter = c ;
+    int iCounter = r -1;
 
+    int totalj = 0;
+    int totali = 0;
+    int iLoop = 0;
+    int jLoop = 0;
+    int maxElements = r * c ;
+    int currentElement = 0;
+    int i =0 ;
+    int j = -1;
+    int jOffset = 1;
+    int iOffset = 1;
+    //res.push_back(matrix[i][j]);
+    while(currentElement < maxElements)
+    {
+        while(totalj < jCounter)
+        {
+            j += jOffset;
+            res.push_back(matrix[i][j]);
+            totalj++;
+            currentElement++;
+        }        
+        while(totali < iCounter )
+        {
+            i += iOffset;
+            res.push_back(matrix[i][j]);
+            totali++;
+            currentElement++;
+        }
+        jCounter--;
+        iCounter--;      
+        totali = 0;
+        totalj = 0;
+        if(jOffset >0)
+        {
+            jOffset = -1;
+        }
+        else
+        {
+            jOffset = 1;
+            
+        
+        }
+        if(iOffset > 0)
+        {
+            iOffset = -1;
+            
+        }
+        else
+        {
+            iOffset = 1;
+            
+        }
     }
-    
+
+    return res;    
 }
 
 int main()
 {
-
+    vector<vector<int>> matrix = {{1,2,3,4} , {5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    vector<int> res = spirallyTraverse(matrix  , 4 , 4);
+    for (int i = 0; i < res.size(); i++)
+    {
+        
+            cout <<  res[i]<<" ";
+        
+    }
+    
+    
 }
 
 //1  2  3  4
 //5  6  7  8
 //9  10 11 12
-//13 14 16 17
+//13 14 15 16
+
